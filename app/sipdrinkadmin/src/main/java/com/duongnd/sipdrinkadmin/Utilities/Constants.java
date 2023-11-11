078b802dcf7432934ab406fd192ba521721a9605
hlongt109 <hlong109.it@gmail.com>
2023-11-11 23:36:12 +0700

    update gdien Login,SignUp cho Admin

---
+++ app/sipdrinkadmin/src/main/java/com/duongnd/sipdrinkadmin/Utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.duongnd.sipdrinkadmin.Utilities;
+
+public class Constants {
+    public static final String Key_Colection_Admin_table = "Admin";
+    public static final String Key_Id_Admin = "id";
+    public static final String Key_Username = "username";
+    public static final String Key_Password = "password";
+    public static final String Key_Name = "fullName";
+    public static final String Key_Email = "email";
+    public static final String Key_Is_Logged = "isLogged";
+    public static final String KEY_PREFERENCE_NAME = "AdminPreference";
+}
